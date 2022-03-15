527c2e390c9232bed5b67d95567aacccb8288b9a
duduzx02 <duduzx02@gmail.com>
2022-03-14 23:37:10 -0300

    ...

---
+++ Include/curso/Aula02_Comando_print.py
@@ -0,0 +1,9 @@
+# funções executa ações
+# print() serve para exibir alguma coisa na tela para o usuário,
+
+print(123456)
+print('Eduardo', 'Lima', end='&')  # Tudo que estiver dentro dos parênteses são argumentos
+                        # A função end='' junta a proxima linha da linha anterior
+print('Eduardo', 'Lima', sep='+')  # A função sep='' é utilizado para colocar algo entre os argumentos separados por ,
+
+# o Python diferencia as letras MAIÚSCULA e minuscula
