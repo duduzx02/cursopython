e76e718c47c3dd6406e049390ab5c6781ef6d3ed
duduzx02 <duduzx02@gmail.com>
2022-03-14 23:12:10 -0300

    Initial commit

---
+++ Include/curso/aula01_Comentários.py
@@ -0,0 +1,11 @@
+""" Outra forma utilizada como comentário"""
+
+
+# Isso é um comentário
+print('Linha 1')
+print('Linha 2')
+print('Linha 3')  # Falando sobre do que se trata essa linha
+print('Linha 4')  # Para comentar na mesma linha do código o python depe dois space
+print('Linha 5')
+print('Linha 6')
+# Os comentários não apareceram nos terminal
