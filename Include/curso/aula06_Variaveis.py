e333d1695ef60d3db6f5cbc868cb98b0a0fb33f3
duduzx02 <duduzx02@gmail.com>
2022-03-15 03:51:10 -0300

    ...

---
+++ Include/curso/aula06_Variaveis.py
@@ -0,0 +1,32 @@
+'''
+
+inicar com letra, pode conter número, separar _, letras minúsculas
+
+
+'''
+
+nome = 'Eduardo Lima'
+idade = 26
+altura = 1.76
+e_maior = idade > 18
+data_atual = 2022
+peso = 95
+imc = peso / (altura ** 2)
+
+print('Nome:', nome)
+print('Idade:', idade)
+print('Altura:', altura)
+print('É maior:', e_maior)
+print('Data atual:', data_atual)
+print('Peso', peso)
+print(nome, 'Tem', idade, 'anos de idade e seu imc é', imc)
+
+'''
+Strings são qualquer texto entre aspas simples, 
+aspas duplas, aspas simples triplas e aspas duplas 
+triplas. Int são números inteiros negativos ou 
+positivos (fora de aspas). Float são números com 
+ponto flutuante (fora de aspas). Valores booleanos
+(lógicos) só podem ter dois valores, True 
+para verdadeiro e False para falso.
+'''
