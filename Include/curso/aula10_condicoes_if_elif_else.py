15362c4184eb3d0a9ddf396fc7a7bf427c53a15e
duduzx02 <duduzx02@gmail.com>
2022-03-17 00:53:53 -0300

    ...

---
+++ Include/curso/aula10_condicoes_if_elif_else.py
@@ -0,0 +1,14 @@
+'''
+
+Condições IF, ELIF e ELSE
+
+'''
+
+if False:
+    print('Verdadeiro.')
+elif False:
+    print('Agora é verdadeiro.')
+elif False:
+    print('Mais um verdadeiro.')
+else:
+    print('Não é verdadeiro')
