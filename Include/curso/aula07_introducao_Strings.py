7b84043e90dfa5e05854d01663f40aa512cd3d40
duduzx02 <duduzx02@gmail.com>
2022-03-16 22:39:16 -0300

    ...

---
+++ Include/curso/aula07_introducao_Strings.py
@@ -0,0 +1,8 @@
+nome = 'Eduardo Lima'
+idade = 26
+altura = 1.76
+peso = 95
+imc = peso / (altura ** 2)
+
+print(f'{nome} tem {idade} anos de idade e seu imc é {imc:.2f}')
+print('{no} tem {id} anos de idade e seu imc é {imc:.2f}'.format(no = nome, id = idade, imc = imc))
