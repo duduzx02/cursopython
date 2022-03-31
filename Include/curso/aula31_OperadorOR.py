29d21794db8ad69cbfb848559ff36598b6bb8728
duduzx02 <duduzx02@gmail.com>
2022-03-31 02:50:24 -0300

    ...

---
+++ Include/curso/aula31_OperadorOR.py
@@ -0,0 +1,15 @@
+'''
+nome = input('Qual o seu nome? ')
+print(nome or 'Você não digitou nada.')
+'''
+
+a = 0
+b = None
+c = False
+d = []
+e = {}
+f = 22
+g = 'Eduardo'
+
+tudo = a or b or c or d or e or f or g
+print(tudo)
