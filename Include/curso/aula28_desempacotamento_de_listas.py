370d053132db467b3b0b3423bc1950c227b07667
duduzx02 <duduzx02@gmail.com>
2022-03-31 01:43:34 -0300

    ...

---
+++ Include/curso/aula28_desempacotamento_de_listas.py
@@ -0,0 +1,14 @@
+'''
+
+desempacotamento de listas
+
+'''
+
+lista = ['Du', 'Dudu', 'Edu', 1, 2, 3]
+
+n1, n2, *infinito, penultimo, ultimo = lista
+print(n1)
+print(n1, n2)
+print(infinito)
+print(penultimo)
+print(ultimo)
