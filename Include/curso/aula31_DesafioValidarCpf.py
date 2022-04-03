8f89c9350edca2fccf5744a7b20af976f198cfa1
duduzx02 <duduzx02@gmail.com>
2022-04-02 22:46:57 -0300

    ...

--- Include/curso/aula31_DesafioValidarCpf.py
+++ Include/curso/aula31_DesafioValidarCpf.py
@@ -1,17 +1,12 @@
-cpf = input('digite seu cpf: ')
+cpf = input('Digite seu cpf: ')
 total = 0
 for indice, contador in enumerate(range(10, 1, -1)):
     num = int(cpf[indice])
     total = total + (num * contador)
-    print(num, contador)
-print(total)
+
 if total == 297:
     total = 0
     cpf = input('Digite novamente seu cpf: ')
     for indice, contador in enumerate(range(11, 1, -1)):
         num = int(cpf[indice])
         total = total + (num * contador)
-        print(num, contador)
-print(total)
-
-
