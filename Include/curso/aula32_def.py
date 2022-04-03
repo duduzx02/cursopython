8ec46df903fcc7b1dfabc6a0deda1ce077de4ffb
duduzx02 <duduzx02@gmail.com>
2022-04-02 23:39:25 -0300

    ...

---
+++ Include/curso/aula32_def.py
@@ -0,0 +1,34 @@
+'''
+
+Funções - def
+
+'''
+
+'''
+
+def funcao():
+    print('Hello World!')
+
+funcao()
+funcao()
+
+'''
+
+
+
+'''
+
+def saudadao(msg = input('Digite algo:'), msg2 = input('Digite algo:')):
+    return f'{msg}, {msg2}'
+
+variavel = saudadao()
+print(variavel)
+
+'''
+
+
+
+
+
+
+
